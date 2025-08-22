ca177b2370b4d956ca51a24fcc48fb29ea2637fd
Ocirne <153968073+enriconovellino@users.noreply.github.com>
2025-08-22 09:52:18 -0300

    Alterando a classe Banco

--- src/Banco.java
+++ src/Banco.java
@@ -1,3 +1,17 @@
-public class Banco {
-                                                            // só um teste
+public class Banco implements Pix, Boleto, Cartao{
+    @Override
+    public void processarPagamentoBoleto() {
+
+    }
+
+    @Override
+    public void processarPagamentoCartao() {
+
+    }
+
+    @Override
+    public void processarPagamentoPix() {
+
+    } //Só um teste
+
 }
